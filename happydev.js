1d60040618cac9b54e908964d233aa4b13bb36de
balessan <benoit.alessandroni@gmail.com>
2016-02-05 17:47:19 +0100

    Adding links and names to the nodes

--- happydev.js
+++ happydev.js
@@ -1,31 +1,38 @@
 function getLineLength(line) {
     return Math.sqrt(Math.pow(line.x2 - line.x1, 2) + Math.pow(line.y2 - line.y1, 2))
 }
+
 function drawNodes(container, data) {
     var nodes = container.selectAll(".node")
         .data(data)
-        .enter().append("svg")
-            .attr("class", "node");
+        .enter().append("svg");
 
     // addition d'un Circle à chaque svg.node
     nodes.append("circle")
         .attr("cx", function(d) { return d.cx;})
         .attr("cy", function(d) { return d.cy;})
-        .attr("r", function(d) { return d.name ? 16 : 4; });
-        
+        .attr("r", function(d) { return d.name ? 16 : 4; })
+        .on("click", function(d) {
+          window.location.hash = d.url;
+        });
+
     // addition d'un Text à chaque svg.node
     nodes.each(function(d) {
-        if (d.name)
+        if (d.name) {
             d3.select(this).append("text")
                 .attr("class", "node-name")
                 .attr("x", function(d) { return d.cx-38; })
                 .attr("y", function(d) { return d.cy+55; })
-                .text(function(d) { return d.name; });
+                .text(function(d) { return d.name; })
+                .on("click", function(d) {
+                  window.location.hash = d.url;
+                });
+        }
     });
     return nodes;
 }
-    
-function drawLinks(container, data) { 
+
+function drawLinks(container, data) {
     var links = container.selectAll(".link")
         .data(data)
         .enter().append("line")
