05501b1c28ff09e7263da8e5c1d19a0f6bcfd7f2
JonathanBraley <jonathan.b@happy-dev.fr>
2016-04-16 16:47:12 +0200

    Update: contact form now interactive

--- happydev.js
+++ happydev.js
@@ -140,9 +140,17 @@ function showMember(member) {
 }
 
 function sendContact() {
-    alert('salut ' + $("#contact-name").val() + ' !');
-    $('#contact').hide();
-    return false;
+    if(contactEtape != 3){
+		return false;
+	}else{
+		alert('salut ' + $("#contact-name").val() + ' !');
+		$('#contact').hide();
+		$("#contact-name, #contact-howru, #contact-request, #contact-contact").val("");
+		contactEtape = 0;
+		$('#contactEtape1, #contactEtape2, #contactEtape3').hide();
+		$(".contact-centre").remove();
+		return false;
+	}
 }
 
 function hidePanel(){
@@ -177,5 +185,50 @@ $(function() {
 		$("#manifeste").slideUp("slow");
 	});
 	
+	contactEtape = 0;
+	
+	$("#contact .back").on("click", function(){
+		$("#contact").hide();
+	});
+	
+	$("#contact-name").on("change", function(){
+		$("#contactEtape1").show();
+		if(contactEtape == 0){
+			$("<div class='contact-centre'>Enchanté "+ $(this).val() +", nous sommes HAPPY DEV!</div>").insertBefore("#contactEtape1");
+			contactEtape += 1;
+		}else{
+			$(this).closest("div").next(".contact-centre").remove();
+			$("<div class='contact-centre'>Enchanté "+ $(this).val() +", nous sommes HAPPY DEV!</div>").insertBefore("#contactEtape1");
+		}
+	});
+	
+	$("#contact-howru").on("change", function(){
+		$("#contactEtape2").show();
+		if(contactEtape == 1){
+			contactEtape += 1;
+		}
+	});
+	
+	$("#contact-request").on("change",function(){
+		$("#contactEtape3").show();
+		if(contactEtape == 2){
+			$("<div class='contact-centre'>Ta demande va être prise en compte dans les meilleurs délais.</div>").insertBefore("#contactEtape3");
+			contactEtape +=1;
+		}
+	});
+	
+	$("#contact-contact").on("change",function(){
+		if(isValidEmailAddress($(this).val())){
+			$(this).closest("form").submit();
+		}else{
+			$(this).closest("form").append("<div class='contact-centre'>Cet email n'est pas valide!</div>");
+		}
+	});
+	
 	$("#unzoom").on("click",unZoom);
 });
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
