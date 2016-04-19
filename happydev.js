f292c2983dd27392f235032da450abc5a45d4e4b
JonathanBraley <jonathan.b@happy-dev.fr>
2016-04-19 11:02:51 +0200

    Update: routing using history.js in replacement of hasher.js

--- happydev.js
+++ happydev.js
@@ -123,7 +123,7 @@ function zoom(node) {
             $(node.div).show();
 			$("#manifeste").hide();
         }
-		setHashSilently(node.name+"/");
+		History.pushState(null,"Happy"+node.name,node.name);
     }
 }
 
@@ -136,7 +136,7 @@ function unZoom(){
 	$(".happy-title").show("slow");
 	$(".happy-title").animate({left:"50%",top:"50%",width:400,height:200,marginLeft:"-200px",marginTop:"-100px"});
 	
-	setHashSilently("");
+	History.pushState(null,"HappyHome","/");
 }
 
 function showMember(member) {
@@ -155,7 +155,7 @@ function sendContact() {
 		$('#contactEtape1, #contactEtape2, #contactEtape3').hide();
 		$(".contact-centre").remove();
 		return false;
-		setHashSilently("");
+		History.pushState(null,"HappyHome","/");
 	}
 }
 
@@ -185,19 +185,19 @@ $(function() {
 	
 	$(".happy-title").on("click", function(){
 		$("#manifeste").slideDown("slow");
-		setHashSilently("manifeste/");
+		History.pushState(null,"HappyManifesto","manifeste");
 	});
 	
 	$("#manifeste").on("click", function(){
 		$("#manifeste").slideUp("slow");
-		setHashSilently("");
+		History.pushState(null,"HappyHome","/");
 	});
 	
 	contactEtape = 0;
 	
 	$("#contact .back").on("click", function(){
 		$("#contact").hide();
-		setHashSilently("");
+		History.pushState(null,"HappyHome","/");
 	});
 	
 	$("#contact-name").on("change", function(){
@@ -236,58 +236,47 @@ $(function() {
 	
 	$("#unzoom").on("click",unZoom);
 	
-	var route1 = crossroads.addRoute("");
-	var route2 = crossroads.addRoute("team/");
-	var route3 = crossroads.addRoute("projects/");
-	var route4 = crossroads.addRoute("manifeste/");
-	var route5 = crossroads.addRoute("contact/");
+	var route1 = crossroads.addRoute("team");
+	var route2 = crossroads.addRoute("projects");
+	var route3 = crossroads.addRoute("manifeste");
+	var route4 = crossroads.addRoute("contact");
+	
+	var states = History.getState();
+	
+	var url = states.url;
 	
 	route1.matched.add(function(){
-		unZoom;
-		$("#contact").hide();
-		$("#manifeste").slideUp("slow");
-	});
-	
-	route2.matched.add(function(){
 		d3.json("data.json",function(data){
 			zoom(data.nodes[2]);
 		});
 	});
 	
-	route3.matched.add(function(){
+	route2.matched.add(function(){
 		d3.json("data.json",function(data){
 			zoom(data.nodes[4]);
 		});
 	});
 	
-	route4.matched.add(function(){
+	route3.matched.add(function(){
 		unZoom;
 		$("#contact").hide();
 		$("#manifeste").slideDown("slow");
 	});
 	
-	route5.matched.add(function(){
+	route4.matched.add(function(){
 		unZoom;
 		$("#contact").show();
 		$("#manifeste").slideUp("slow");
 	});
 	
-	function parseHash(newHash, oldHash){
-		crossroads.parse(newHash);
-	}
+	if(url.length > 4){
+		var resUrl = url.split("/");
 	
-	hasher.initialized.add(parseHash);
-	hasher.changed.add(parseHash);
-	hasher.init();
+		crossroads.parse(resUrl[4]);
+	}
 });
 
 function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
 };
-
-function setHashSilently(hash){
-  hasher.changed.active = false;
-  hasher.setHash(hash);
-  hasher.changed.active = true;
-}
